a1fd3fa8b094f558f3d42f20b9d70da3f536c659
Edwin Onuonga <ed@hazy.com>
2023-03-30 03:10:58 +0100

    fix kwargs

--- test_db.py
+++ test_db.py
@@ -9,11 +9,10 @@ from testcontainers.mssql import SqlServerContainer
 
 @pytest.fixture(scope="module")
 def db2_engine():
-    container = Db2Container(
-        "ibmcom/db2:latest",
-        platform="linux/amd64",
-        privileged=True,
-    )  # .with_env("DOCKER_HOST", os.environ["DOCKER_HOST"])
+    container = Db2Container("ibmcom/db2:latest").with_kwargs(
+        platform="linux/amd64", privileged=True
+    )
+    # .with_env("DOCKER_HOST", os.environ["DOCKER_HOST"])
     with container as db2:
         engine = sqlalchemy.create_engine(db2.get_connection_url())
         yield engine
@@ -23,9 +22,9 @@ def db2_engine():
 @pytest.fixture(scope="module")
 def mssql_engine():
     container = SqlServerContainer(
-        "mcr.microsoft.com/mssql/server:2017-latest",
-        platform="linux/amd64",
-    )  # .with_env("DOCKER_HOST", os.environ["DOCKER_HOST"])
+        "mcr.microsoft.com/mssql/server:2017-latest"
+    ).with_kwargs(platform="linux/amd64")
+    # .with_env("DOCKER_HOST", os.environ["DOCKER_HOST"])
     with container as mssql:
         engine = sqlalchemy.create_engine(mssql.get_connection_url())
         yield engine
