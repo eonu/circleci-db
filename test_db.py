bb5119fe88907165c15597b52d37156ef2784661
Edwin Onuonga <ed@hazy.com>
2023-03-30 03:03:53 +0100

    remove host

--- test_db.py
+++ test_db.py
@@ -13,7 +13,7 @@ def db2_engine():
         "ibmcom/db2:latest",
         platform="linux/amd64",
         privileged=True,
-    ).with_env("DOCKER_HOST", os.environ["DOCKER_HOST"])
+    )  # .with_env("DOCKER_HOST", os.environ["DOCKER_HOST"])
     with container as db2:
         engine = sqlalchemy.create_engine(db2.get_connection_url())
         yield engine
@@ -25,7 +25,7 @@ def mssql_engine():
     container = SqlServerContainer(
         "mcr.microsoft.com/mssql/server:2017-latest",
         platform="linux/amd64",
-    ).with_env("DOCKER_HOST", os.environ["DOCKER_HOST"])
+    )  # .with_env("DOCKER_HOST", os.environ["DOCKER_HOST"])
     with container as mssql:
         engine = sqlalchemy.create_engine(mssql.get_connection_url())
         yield engine
